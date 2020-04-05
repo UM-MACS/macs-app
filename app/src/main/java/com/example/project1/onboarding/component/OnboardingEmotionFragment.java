bb574f9871fa26a2e0e87a09d4f296e35a3500e0
chiajee <leechia.jee97@gmail.com>
2020-04-05 16:53:26 +0800

    added onboarding for forum and emotion

--- app/src/main/java/com/example/project1/onboarding/component/OnboardingEmotionFragment.java
+++ app/src/main/java/com/example/project1/onboarding/component/OnboardingEmotionFragment.java
@@ -2,23 +2,13 @@ package com.example.project1.onboarding.component;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
+
 
 import com.example.project1.R;
-import com.example.project1.emotionAssessment.component.EmotionFragment;
-import com.example.project1.login.component.SessionManager;
-import com.example.project1.login.component.User;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 
 public class OnboardingEmotionFragment extends Fragment {
     public OnboardingEmotionFragment(){}
