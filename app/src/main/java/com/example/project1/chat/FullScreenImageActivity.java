092abba8c1d49a5e64b0b32225cc6598a01917b2
cheiw256 <cheiw256@gmail.com>
2021-07-27 00:36:38 +0800

    Add Zoomable features to Photo View in Chat Features

--- app/src/main/java/com/example/project1/chat/FullScreenImageActivity.java
+++ app/src/main/java/com/example/project1/chat/FullScreenImageActivity.java
@@ -9,15 +9,17 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.example.project1.R;
+import com.github.chrisbanes.photoview.PhotoView;
 
 public class FullScreenImageActivity extends AppCompatActivity {
+    private PhotoView fullScreenImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image);
         
-        ImageView fullScreenImageView = (ImageView) findViewById(R.id.fullScreenImageView);
+        fullScreenImageView = findViewById(R.id.fullScreenImageView);
         Intent callingActivityIntent = getIntent();
         if (callingActivityIntent != null) {
             Uri imageUri = callingActivityIntent.getData();
